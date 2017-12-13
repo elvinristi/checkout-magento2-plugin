17893fe2ff381772d49ac6e43474de5f3bb8935a
Elvin Risti <elvin.risti@vaimo.com>
2017-12-13 21:58:19 +0200

    fix email_order_set_template_vars_before observer

--- Observer/AddExtraDataToTransport.php
+++ Observer/AddExtraDataToTransport.php
@@ -14,19 +14,45 @@ use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 
-class AddExtraDataToTransport implements ObserverInterface {
+class AddExtraDataToTransport implements ObserverInterface
+{
+    const XPATH_PAYMENT_TITLE = 'payment/checkout_com/title';
 
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * AddExtraDataToTransport constructor.
+     *
+     * @param ScopeConfigInterface $scopeConfig
+     */
     public function __construct(ScopeConfigInterface $scopeConfig)
     {
         $this->scopeConfig = $scopeConfig;
     }
 
+    /**
+     * @event email_order_set_template_vars_before
+     * @param Observer $observer
+     */
     public function execute(Observer $observer)
     {
-        // Get the email content
+        // Get the email sender transport
+        /** @var \Magento\Framework\DataObject $transport */
         $transport = $observer->getEvent()->getTransport();
 
-        // Override the payment information block
-        $transport['payment_html'] = $this->scopeConfig->getValue('payment/checkout_com/title');
+        // Get the current payment method used
+        $paymentMethod = $transport->getOrder()->getPayment()->getMethod();
+
+        if (in_array($paymentMethod, ['checkout_com', 'checkout_com_cc_vault'])) {
+            // Override the payment information block
+            $transport->setData('payment_html', $this->scopeConfig->getValue(
+                self::XPATH_PAYMENT_TITLE,
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+                $transport->getStore()
+            ));
+        }
     }
 }
